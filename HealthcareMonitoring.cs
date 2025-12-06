db6bc22f2ab3cefd7ae91437561e4035783b4ac2
Vimalsankar14 <162667094+Vimalsankar14@users.noreply.github.com>
2025-12-06 12:08:37 +0530

    Add files via upload

---
+++ HealthcareMonitoring.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace HealthcareMonitoring
+{
+    class TemperatureModule
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Reading temperature sensor values.");
+
+            VibrationModule.RunVibrationCheck();
+
+            Console.WriteLine("Temperature Module Execution Completed.");
+            Console.ReadLine();
+        }
+    }
+
+    class VibrationModule
+    {
+        public static void RunVibrationCheck()
+        {
+            Console.WriteLine("Vibration Module Executing.");
+
+            Console.WriteLine("Vibration Module Task Completed.");
+        }
+    }
+}
