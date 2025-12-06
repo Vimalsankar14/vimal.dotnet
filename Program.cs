8aa21d87f4ce5d1e19ffb6eccab014a3c15c9036
Vimalsankar14 <162667094+Vimalsankar14@users.noreply.github.com>
2025-12-06 11:48:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class SmallestValueProgram
+{
+    static void Main()
+    {
+        Console.Write("Enter how many numbers: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[n];
+
+        Console.WriteLine("\nEnter the numbers:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Number {i + 1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int smallest = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < smallest)
+                smallest = numbers[i];
+        }
+
+        Console.WriteLine($"\nSmallest number: {smallest}");
+
+        Console.ReadLine();
+    }
+}
