7d22f95407b8dc0ac29d7007c113d1121440ea5e
Vimalsankar14 <162667094+Vimalsankar14@users.noreply.github.com>
2025-12-06 11:55:58 +0530

    Add files via upload

---
+++ datatypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class TransactionDemo
+{
+    static void Main()
+    {
+        string transactionId = "TRX123456";
+        decimal amount = 1000000.25m;
+        bool isinternational = true;
+        DateTime transactiontimestamp = DateTime.Now;
+        int rewardpoints = 1500;
+
+        Console.WriteLine("Transaction ID:{0}", transactionId);
+        Console.WriteLine("Amount: {0}", amount);
+        Console.WriteLine("Is International:{0}", isinternational);
+        Console.WriteLine("Transaction Timestamp:{0}", transactiontimestamp);
+        Console.WriteLine("Reward Points: {0}", rewardpoints);
+    }
+
+}
